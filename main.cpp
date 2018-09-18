a942d59407e1e7bad96ff42b66ec7789d1a5969b
royeking7 <rking27@mail.csuchico.edu>
2018-09-17 17:09:59 -0700

    Lucky number 7 game

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Author:
+//Author:Roy King
 #include <iostream>
 
 using namespace std;
@@ -10,15 +10,20 @@ int main()
   cout<<"Guess a number between 1-10: ";
   cin>>guess;
 
-
+  if(guess==7)
+  {
   //when they guess correctly (7), display:
   cout<<"YES! Lucky number 7!\n";
-
+  }
+  else if(guess==6||guess==8)
+  {
   //when they guess just off by one (either 6 or 8), display:
   cout<<"Oh, so close!\n";
-
+  }
+  else
+  {
   //when they guess any other number, display:
   cout<<"Waaay off, sorry.\n";
-
+  }
   return 0;
 }
